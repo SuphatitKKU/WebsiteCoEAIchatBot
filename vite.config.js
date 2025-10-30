b1c797f38bb606a3c41776b611dee5031a3bd05f
SuphatitKKU <144255039+SuphatitKKU@users.noreply.github.com>
2025-10-31 01:24:12 +0700

    update vitconfig

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  console.log('🔍 Mode:', mode)
-  console.log('🔍 Base URL:', env.VITE_BASE_URL)
-  
-  return {
-    plugins: [react(), tailwindcss()],
-    base: env.VITE_BASE_URL || '/'
-  }
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/codme/'
 })
-
