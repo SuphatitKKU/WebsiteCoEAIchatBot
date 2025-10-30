efd0fdecb7e845c4c61471c192235b4a634d2e92
SuphatitKKU <144255039+SuphatitKKU@users.noreply.github.com>
2025-10-31 01:07:52 +0700

    vit update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   
+  console.log('🔍 Mode:', mode)
+  console.log('🔍 Base URL:', env.VITE_BASE_URL)
+  
   return {
     plugins: [react(), tailwindcss()],
     base: env.VITE_BASE_URL || '/'
