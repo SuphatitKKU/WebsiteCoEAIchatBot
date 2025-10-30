a9e889362e8eabea4e36616937063a5931ae91d5
SuphatitKKU <144255039+SuphatitKKU@users.noreply.github.com>
2025-10-31 01:04:05 +0700

    ีupdate vit config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/codeme/' : '/'
-
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    base: env.VITE_BASE_URL || '/'
+  }
 })
